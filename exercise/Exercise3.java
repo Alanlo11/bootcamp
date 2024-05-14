574154b0028d187788a09f59b97ffcd8fdad91d7
Alanlo <lun0628@hotmail.com>
2024-05-15 00:42:25 +0800

    exercise

--- exercise/Exercise3.java
+++ exercise/Exercise3.java
@@ -2,7 +2,7 @@ package exercise;
 import java.text.BreakIterator;
 import java.util.Scanner;
 
-public class Excercise3 {
+public class Exercise3 {
     public static void main(String[] args) {
           /**
            * Expected Output:
