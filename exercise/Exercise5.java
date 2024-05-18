42ea6ce58c4de5d21a3cbe4eda7a544db051a234
Alanlo <lun0628@hotmail.com>
2024-05-18 23:52:40 +0800

    exercise

--- exercise/Exercise5.java
+++ exercise/Exercise5.java
@@ -12,17 +12,20 @@ public class Exercise5 {
            */
           // Count the target of character in a String.
             String str = "Welcome to the coding bootcamp."; // You should not change this line
-            char target = 'c'; // Update this target to test the logic
+            char target = 'z'; // Update this target to test the logic
         
             // Use a loop to count the target of character in a String.
             // code here ...
             int i=0;
+            int j = 0;
         for(i=0 ; i< str.length() ; i++) {
             if(str.charAt(i) != target)
-            System.out.println("We found "+i+" character c in the String");
+            {continue;}
+            j += 1;}
+            if(j>0)
+            System.out.println("We found "+j+" character c in the String");
             else
             System.out.println("Not Found");
-        }
             // if found, print "We found 3 character c in the String."
             // if not found, print "Not Found."
             // code here ...
