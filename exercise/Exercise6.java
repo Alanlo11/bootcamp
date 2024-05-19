cf3245a993a1220379d8a78c5263ff65d7a1a4ef
Alanlo <lun0628@hotmail.com>
2024-05-19 14:21:48 +0800

    exercise

--- exercise/Exercise6.java
+++ exercise/Exercise6.java
@@ -11,5 +11,16 @@ public class Exercise6 {
     // Code here ...
     // for loop to print first 15 numbers in Fibonacci Sequence
 
+    int n1 = 1;
+    int n2 = 1;
+    int n3 = 0;
+    System.out.println(n1);
+    System.out.println(n2);
+    for (int i=2 ; i<15 ; i++){
+      n3=n1+n2;
+      System.out.println(n3);
+      n1=n2;
+      n2=n3;
+    }
   }
 }
