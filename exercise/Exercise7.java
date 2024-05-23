bd44efb4ebeb750249cc9db346719fc5bc7b5f33
Alanlo <lun0628@hotmail.com>
2024-05-24 02:57:34 +0800

    update

--- exercise/Exercise7.java
+++ exercise/Exercise7.java
@@ -9,14 +9,16 @@ public class Exercise7 {
             // Declare a int array, with value 1 to 10
             // code here
             int sum = 0;
-            int[] a = new int [] {1,2,3,4,5,6,7,8,9,10};
+            int[] a = new int [11];
+            for(int i=0 ; i<a.length ; i++){
+              a[i] = i;
+            }
+        
+            // Write a loop to sum up all value in the int array
+            // code here ...
             for(int i=0 ; i<a.length ; i++){
               sum += a[i];
             }
             System.out.println(sum);
-        
-            // Write a loop to sum up all value in the int array
-            // code here ...
-          
           }
         }
